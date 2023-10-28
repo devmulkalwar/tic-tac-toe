5775d9bc607ac737e05ffe9bebeea4b76ce4d51c
devmulkalwar <devmulkalwar95@gmail.com>
2023-10-29 02:40:41 +0530

    win functionality done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-const turnElement = document.querySelector(".turn-div")
-turnElement.innerHTML = `<h3>Player X Turn </h3>`;
 let turn = "X";
-let gameCount = 0;
 let playerXScore = 0;
 let playerOScore = 0;
+let gameCount = 0;
 let isGameOver = false;
 
-//function to change the turn
+//changes players turn
 const changeTurn = () => {
-    return turn === "X" ? "O" : "X"
+    return turn === "X" ? "O" : "X";
 }
 
-//function to check win 
+//checks for winner
 const checkWin = () => {
-    let boxtext = document.getElementsByClassName("box-text");
-    let wins = [
+    const wins = [
         [0, 1, 2],
         [3, 4, 5],
         [6, 7, 8],
@@ -24,40 +21,49 @@ const checkWin = () => {
         [0, 4, 8],
         [2, 4, 6],
     ]
-    wins.forEach(e => {
-        if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")) {
-            document.querySelector(".result-div").innerHTML = `<h3>Player ${boxtext[e[0]].innerText} Won!!</h3>`;
-            if (boxtext[e[0]].innerText === "X") {
-                playerXScore++;
-                document.querySelector(".player-x-div").innerHTML += `<h3>${playerXScore}</h3>`
+
+
+    for (let i = 0; i < 8; i++) {
+
+        let idx1 = wins[i][0];
+        let idx2 = wins[i][1];
+        let idx3 = wins[i][2];
+
+        if((boxes[idx1].innerText===boxes[idx2].innerText)&&(boxes[idx2].innerText===boxes[idx3].innerText))
+        {
+            if((boxes[idx1].innerText!=="")&&(boxes[idx2].innerText!=="")&&(boxes[idx3].innerText!==""))
+            {
+                document.querySelector(".result-div").innerHTML=`<h3> Player ${boxes[idx1].innerText} won !!</h3>`
+                console.log(`Player ${boxes[idx1].innerText}  won`)
+                isGameOver = true;
             }
-            else {
-                playerOScore++;
-                document.querySelector(".player-o-div").innerHTML += `<h3>${playerOScore}</h3>`
-            }
-            gameCount++;
-            isGameOver = true;
         }
-    })
+
+    }
 }
 
-//function to reset game
-const reset = () => {
+let boxes = Array.from(document.querySelectorAll(".game-box"));
 
-}
-
-let boxes = document.getElementsByClassName("game-box");
-console.log(boxes);
-Array.from(boxes).forEach((element) => {
-    let boxtext = element.querySelector(".box-text");
+for (let i = 0; i < 9; i++) {
+    const element = boxes[i];
+    let isClicked = isGameOver?true:false;
+  
     element.addEventListener("click", () => {
-        if (boxtext.innerText === "") {
-            boxtext.innerText = turn;
-            turn = changeTurn();
-            checkWin();
-            if (!isGameOver) {
-                turnElement.innerHTML = `<h3>Player ${turn} Turn </h3>`;
+        console.log(`box ${i + 1} clicked!!`);
+        if (!isClicked) {
+            if (element.innerText === "") {
+                element.innerText = turn;
             }
+            checkWin();
+
+            if (!isGameOver) {
+                turn = changeTurn();
+                document.querySelector(".turn-div").innerHTML = `<h3>Player ${turn} turn </h3>`
+            }
+            isClicked = true;
+        }
+        else {
+            console.log("element is already clicked");
         }
     })
-});
+}
