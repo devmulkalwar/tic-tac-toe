3ca91c73de7b41bf74425ea613d9253fb430da1d
devmulkalwar <devmulkalwar95@gmail.com>
2023-12-12 14:17:45 +0530

    code update

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ let playerOScore = 0;
 let gameCount = 0;
 let moveCount = 0;
 let isGameOver = false;
-
+let currentMode = 2;
 let boxes = Array.from(document.querySelectorAll(".game-box"));
+
+console.log("Welcome to tic tac toe game");
 
 //changes players turn
 const changeTurn = () => {
@@ -96,17 +98,23 @@ const resetGrid = () => {
     document.querySelector(".result-div").innerHTML = "";
     moveCount = 0;
     isGameOver = false;
-    if(gameCount%2 === 0)
-    {
-        turn = "X";
-        document.querySelector(".turn-div").innerHTML = `<h3>Player ${turn} turn </h3>`
-    }
 
-    else{
-        turn = "O";
-        document.querySelector(".turn-div").innerHTML = `<h3>Player ${turn} turn </h3>`
+    // Reset the turn to "X" before updating the display
+    turn = "X";
+
+    if (currentMode === 1) {
+        if (gameCount % 2 === 0) {
+            turn = "X";
+            document.querySelector(".turn-div").innerHTML = `<h3>Player ${turn} turn </h3>`;
+        } else {
+            turn = "O";
+            document.querySelector(".turn-div").innerHTML = `<h3>Player ${turn} turn </h3>`;
+        }
+    } else {
+        vsBotMode();
     }
-}
+};
+
 
 
 // Function to handle box click
@@ -114,23 +122,96 @@ const handleBoxClick = (index) => {
     if (!isGameOver && boxes[index].innerText === "") {
         boxes[index].innerText = turn;
         moveCount++;
-        console.log(moveCount);
+        console.log("move count : " + moveCount);
         checkResult();
 
         if (!isGameOver) {
+
             turn = changeTurn();
-            document.querySelector(".turn-div").innerHTML = `<h3>Player ${turn} turn </h3>`
+            document.querySelector(".turn-div").innerHTML = `<h3>Player ${turn} turn </h3>`;
+            if (moveCount % 2 === 0 && currentMode === 2) {
+                botMoveGenerator();
+            }
         }
-    } else {
+    }
+    else {
         console.log("Invalid move");
     }
 }
 
-// Add event listeners to each box
-for (let i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener("click", () => {
-        console.log(`box ${i + 1} clicked!!`);
-        handleBoxClick(i);
-    });
+// function to check empty boxes
+const findEmptyBox = () => {
+    let index = [];
+    for (let i = 0; i < boxes.length; i++) {
+        if (boxes[i].innerText === "") {
+            index[i] = i;
+        }
+    }
+    return index;
 }
 
+//function to generate bot moves
+const botMoveGenerator = () => {
+    let randomMove = Math.floor(Math.random() * 9);
+    console.log("random move : " + randomMove);
+    if (findEmptyBox().includes(randomMove)) {
+        turn = "X";
+        handleBoxClick(randomMove);
+    }
+    else {
+        botMoveGenerator();
+    }
+}
+
+const vsHumanMode = () => {
+    console.log('Switching to Vs Human Mode');
+    // Add event listeners to each box
+
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("click", () => {
+            console.log(`box ${i + 1} clicked!!`);
+            handleBoxClick(i);
+        });
+    }
+}
+
+const vsBotMode = () => {
+    if (moveCount % 2 === 0) {
+        console.log('Switching to Vs Bot Mode');
+        botMoveGenerator();
+    }
+
+    else {
+        for (let i = 0; i < boxes.length; i++) {
+            boxes[i].addEventListener("click", () => {
+                console.log(`box ${i + 1} clicked!!`);
+                handleBoxClick(i);
+            });
+        }
+    }
+
+}
+
+//function to toggle between vsBotMode and vsHumanMode
+const toggleMode = () => {
+    const button = document.querySelector('.toggle-btn');
+    currentMode = (currentMode === 1) ? 2 : 1;
+
+    if (currentMode === 1) {
+        button.innerText = 'Vs Human Mode';
+        button.classList.add('vs-human-mode');
+        button.classList.remove('vs-bot-mode');
+        resetGrid();
+        reset();
+        vsHumanMode();
+    } else {
+        button.innerText = 'Vs Bot Mode';
+        button.classList.remove('vs-human-mode');
+        button.classList.add('vs-bot-mode');
+        resetGrid();
+        reset();
+        vsBotMode();
+    }
+}
+
+toggleMode();
